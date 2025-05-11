ff40f302d3c9eda595dfa84338888521188a046b
lautaro <f12_12_1997@hotmail.com>
2025-05-11 13:28:36 -0300

    estilos del nav

--- Cliente/Turnos/src/NavBar.tsx
+++ Cliente/Turnos/src/NavBar.tsx
@@ -1,13 +1,21 @@
 // Navbar.jsx
 import React from "react";
+import { useState } from "react";
 import './index.css';
 export default function Navbar({setVentanaElegida}) {
+  const [idBotones, setIdBotones] = useState(["","",""]);
+  function seleccionarVentana(ventana, nroBoton){
+    setVentanaElegida(ventana);
+    let aux = ["","",""];
+    aux[nroBoton] = "selected";
+    setIdBotones(aux);
+  }
   return (
     <nav>
       <ul>
-        <li><button onClick={() => setVentanaElegida("reservar")}>Reservar</button></li>
-        <li><button onClick={() => setVentanaElegida("consultar")}>Consultar</button></li>
-        <li><button onClick={() => setVentanaElegida("modificar")}>Modificar</button></li>
+        <li><button className="btn-nav" id={idBotones[0]} onClick={()=>seleccionarVentana("reservar", 0)}>Reservar</button></li>
+        <li><button className="btn-nav" id={idBotones[1]} onClick={()=>seleccionarVentana("consultar", 1)}>Consultar</button></li>
+        <li><button className="btn-nav" id={idBotones[2]} onClick={()=>seleccionarVentana("modificar", 2)}>Modificar</button></li>
       </ul>
     </nav>
   );
