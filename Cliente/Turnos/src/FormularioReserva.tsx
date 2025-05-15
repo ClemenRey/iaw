8fdc632811dd118896784a7e8db11cb09c3431f7
Clemente Reynier <clementereynier18@gmail.com>
2025-05-15 15:40:37 -0300

    prbando

--- Cliente/Turnos/src/FormularioReserva.tsx
+++ Cliente/Turnos/src/FormularioReserva.tsx
@@ -3,9 +3,7 @@ import { useEffect } from 'react';
 import './index.css';
 import  AlertaReserva from './AlertaReserva';
 
-const API_URL = process.env.REACT_APP_API_URL;
-
-
+const API_URL = import.meta.env.VITE_API_URL;
 
 function FormularioReserva() {
 
--- Cliente/Turnos/src/PanelModificar.tsx
+++ Cliente/Turnos/src/PanelModificar.tsx
@@ -22,7 +22,8 @@ function PanelModificar() {
   const [eliminar, setEliminar] = useState(false);
   
 
-  const API_URL = process.env.REACT_APP_API_URL;
+  const API_URL = import.meta.env.VITE_API_URL;
+
 
     useEffect (() => {
     
